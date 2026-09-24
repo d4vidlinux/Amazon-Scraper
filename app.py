8110ff0fc8d9bc2cf5dd5bfa3e3a99f6ec29c0ef
d4vidlinux <davidlucaspereirapinto06@gmail.com>
2026-09-24 20:12:43 -0300

    Scraper

---
+++ app.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+produtos = {
+    "name": str,
+    "price": str
+}
+driver = webdriver.Chrome()
+wait = WebDriverWait(driver, 30)
+
+driver.get("https://amazon.com.br")
+
+wait.until(
+    EC.presence_of_element_located((By.XPATH, "//div[@class='_single-video-card_style_video-container__1hKS1 _single-video-card_style_overlay__3Sx3u']"))
+)
+
+names = wait.until(
+    EC.presence_of_all_elements_located((By.XPATH, "//div[@class='a-section a-spacing-none _cXVhZ_asin-title_16ABS']"))
+)
+
+prices = wait.until(
+    EC.presence_of_all_elements_located((By.XPATH, "//div[@class='a-section a-spacing-none _cXVhZ_asin-title_16ABS']/following-sibling::div"))
+)
+
+produtos["name"] = names
+produtos["price"] = prices
+
+for nome, preco in zip(produtos["name"], produtos["price"]):
+    print("\n", nome.text, preco.text.replace("\n", ","))
+
+
+
+input("\nPress Enter to Exit...")
+
+driver.quit()  
